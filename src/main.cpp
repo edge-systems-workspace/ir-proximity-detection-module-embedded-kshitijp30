c5f67fda442c3af6a7af77f8bb4135e0c4a5d394
Kshitij Singh <kshitij.verma_cs.aiml24@gla.ac.in>
2026-02-20 15:06:36 +0530

    implement IR sensor reading and object detection logic

--- src/main.cpp
+++ src/main.cpp
@@ -17,19 +17,14 @@ void setup() {
     Serial.begin(9600);
 }
 
-
-
 void loop() {
+    int irValue = digitalRead(IR_PIN);
 
-    // TODO 6:
-    // Read digital value from IR sensor
+    if (irValue == LOW) {
+        Serial.println("Object Detected");
+    } else {
+        Serial.println("No Object");
+    }
 
-    // TODO 7:
-    // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
-
-    // TODO 8:
-    // Add small delay (200–500ms)
+    delay(200);
 }
